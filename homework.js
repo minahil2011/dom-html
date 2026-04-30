be4fb92b9d1790822cf432f0fa4cac8cc1373032
MinahilQadeer <minahilabc155@gmail.com>
2026-04-30 19:04:16 +0500

    Create homework.js

---
+++ homework.js
@@ -0,0 +1,6 @@
+function myFunction() {
+  document.getElementById("result").innerHTML ="Hello"
+  document.getElementById("topic").innerHTML = "Dom"
+  document.getElementById("result1").innerHTML = "Welcome!";
+  document.getElementById("demo").innerHTML = "To my Website";
+}
